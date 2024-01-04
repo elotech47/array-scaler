a4661cda692055e045da1a9351d9c3338eea716a
elotech47 <eloghosaefficiency@gmail.com>
2024-01-04 14:06:39 -0800

    first commit

---
+++ tests/test.py
@@ -0,0 +1,16 @@
+import pytest
+from array_scaler import ScalingHelper
+
+def test_scale():
+    assert ScalingHelper.scale(5, 0, 10) == 0.5
+    assert ScalingHelper.scale(0, 0, 10) == 0
+    assert ScalingHelper.scale(10, 0, 10) == 1
+
+def test_rescale():
+    assert ScalingHelper.rescale(0.5, 0, 10) == 5
+    assert ScalingHelper.rescale(0, 0, 10) == 0
+    assert ScalingHelper.rescale(1, 0, 10) == 10
+
+
+if __name__ == "__main__":
+    pytest.main([__file__])
